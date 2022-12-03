febc55a4929209f1d06655a964525ac4a765a096
OpenSourcePundit <shashank00098@gmail.com>
2022-12-04 01:59:18 +0530

    Page 3 wiring and js part done
    hypotenuse html and js created and wired. only the css part is left.

---
+++ hypotenuse.js
@@ -0,0 +1,14 @@
+const inputSides = document.querySelectorAll('.input-side');
+const calculateBtn = document.querySelector('#calculate-btn');
+const output = document.querySelector('#output');
+
+function calculate() {
+    const square = sumOfSquares(Number(inputSides[0].value), Number(inputSides[1].value));
+    const hypotenuse = Math.sqrt(square);
+    output.innerText = "The Length of Hypotenuse is " + hypotenuse;
+}
+function sumOfSquares(a, b) {
+    return a ** 2 + b ** 2;
+}
+
+calculateBtn.addEventListener("click", calculate);
